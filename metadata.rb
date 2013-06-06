cf8b1ee01b5cf9ddf5aa28557e2b620eafb15094
Jim Rosser <jarosser06@gmail.com>
2013-06-06 16:11:20 -0500

    upped version for dev

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jarosser06@arch.tamu.edu'
 license          'MIT'
 description      'Installs/Configures mediacore'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 supports         'ubuntu', '= 12.04'
 
 %w[
